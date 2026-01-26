27827da04c85c2d76cfdfcf4d942460f8af86bcc
caro.straub <carolina.straub@gmail.com>
2026-01-26 12:23:10 -0300

    setup en routes.py

--- back/src/routes.py
+++ back/src/routes.py
@@ -24,17 +24,12 @@ def register():
     if Client.query.filter_by(email=email).first():
         return jsonify({"error":"Este mail ya esta registrado"}), 409
     
-    is_admin = False
-    if email == os.getenv("ADMIN_EMAIL") and password == os.getenv("ADMIN_PASSWORD"):
-        is_admin = True
-    else:
-        return jsonify({"error":"Credenciales incorrectas para Admin"})
-    
+
     client = Client(
         email = email,
         name = name,
         subscribe = subscribe,
-        admin = is_admin
+        admin = False
     )
     client.set_password(password)
 
@@ -50,6 +45,52 @@ def register():
         }), 201
     except Exception as e:
         db.session.rollback()
-        app.logger.error(f"Error en registro: {str(e)}")
         return jsonify({"error":"Error en el servidor"}), 500
     
+@api.route('/setup', methods=['POST'])
+def set_up():
+    
+    email = request.form.get("email")
+    password = request.form.get("password")
+    name = request.form.get("name")
+    setup_token = request.form.get("setup_token")
+    
+
+    expected = os.getenv("SETUP_TOKEN")
+    
+    if Client.query.filter_by(admin=True).first():
+        return jsonify({"error":"El setup ya fue completado"}), 403
+    if not all([email, password, name, setup_token]):
+        return jsonify({"error":"Hay que completar todos los campos obligatorios"}), 400
+    if expected is None:
+        return jsonify({"error":"Setup Token no configurado en servidor"}), 500
+    if setup_token != expected:
+        return jsonify({"error":"El SETUP_TOKEN no coincide"}), 403
+    if Client.query.filter_by(email=email).first():
+        return jsonify({"error":"Este mail ya esta registrado"}), 409
+        
+    email = email.strip().lower()
+    
+
+    client = Client(
+        email = email,
+        name = name,
+        admin = True,
+        subscribe = False
+
+    )
+    client.set_password(password)
+    try:
+        db.session.add(client)
+        db.session.commit()
+        access_token = create_access_token (identity=client.id)
+        return jsonify ({
+            "message": "El administrador ha sido cnofigurado exitosamente",
+            "client": client.serialize(),
+            "access_token": access_token
+        }), 201
+    except Exception as e:
+        db.session.rollback()
+        return jsonify({"error":"Error en el servidor"}), 500
+
+
