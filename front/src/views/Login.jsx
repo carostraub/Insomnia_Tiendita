47073c4bc571115002c71ea6ffb2ba9cfe7b927a
caro.straub <carolina.straub@gmail.com>
2026-02-05 12:08:53 -0300

    base Login

--- front/src/views/Login.jsx
+++ front/src/views/Login.jsx
@@ -1,9 +1,67 @@
-import React from 'react'
+import React, {useState} from 'react'
+import { useAuth } from '../Context/AuthContext'
 
 const Login = () => {
+  const { login } = useAuth();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    await login(email, password);
+  };
+
+
+
+
+
+
   return (
-    <div>Login</div>
-  )
-}
+    <form className="container w-50 mt-1" onSubmit={handleSubmit}>
+      <div className="mb-3">
+        <label htmlFor="exampleInputEmail1" className="form-label">
+          Dirección de email
+        </label>
+        <input
+          type="email"
+          className="form-control"
+          id="exampleInputEmail1"
+          value={email}
+          aria-describedby="emailHelp"
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
+        
+      </div>
+      <div className="mb-3">
+        <label htmlFor="exampleInputPassword1" className="form-label">
+          Contraseña
+        </label>
+        <input
+          type="password"
+          className="form-control"
+          id="exampleInputPassword1"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+      </div>
+      <div className="mb-3 form-check">
+        <input
+          type="checkbox"
+          className="form-check-input"
+          id="exampleCheck1"
+        />
+        <label className="form-check-label" htmlFor="exampleCheck1">
+          Confirmar datos
+        </label>
+      </div>
+      <button type="submit" className="btn btn-light">
+        Iniciar sesión
+      </button>
+    </form>
+  );
+};
+
 
 export default Login
